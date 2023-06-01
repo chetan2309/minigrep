b835b6b06295305648516e88302db6be1c8193b1
chetan.sharma <chetan2309@gmail.com>
2023-06-01 20:08:58 +0530

    Refactored version -- Moved responsibilities across functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
+}
+
+struct Config {
+    query: String,
+    file_path: String,
 }
