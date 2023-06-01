b6f184ff74dd2167664e410269a92f45d7be4411
chetan.sharma <chetan2309@gmail.com>
2023-06-01 20:16:38 +0530

    Implemented Config struct and used Config::new style to call constructor

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,27 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
+    // If I move this line above the line where I am printing config.file_path
+    // then I am getting an error. Although, I am still not sure why I am getting it.
+    // Seems like read_to_string moves the string and then I cannot borrow after it gets moved.
+    // A question here is doesn't println! macros move the string??
     let contents =
         fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        }
+    }
 }
 
 struct Config {
