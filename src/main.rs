bd67fb06e124b91019728bafca966cd2b8717934
chetan.sharma <chetan2309@gmail.com>
2023-06-03 00:05:31 +0530

    Used Result pattern to handle along with process::exit method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -19,14 +23,14 @@ fn main() {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!(" Not enough arguments to proceed")
+            return Err("Not enough arguments to proceed");
         }
-        Config {
+        Ok(Config {
             query: args[1].clone(),
             file_path: args[2].clone(),
-        }
+        })
     }
 }
 
